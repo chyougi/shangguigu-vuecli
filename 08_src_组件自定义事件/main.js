160c5beb1d395778dfbd640f0d7042ce2e3b16fd
chyougi <826604359@qq.com>
2024-09-02 23:47:47 +0900

    9月2日完成

---
+++ 08_src_组件自定义事件/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+
+new Vue({
+    el:'#app',
+    render: h => h(App),
+    // mounted(){
+    //     setTimeout(() => {
+    //         this.$destroy()
+    //     }, 3000);
+    // }
+})
